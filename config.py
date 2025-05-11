e69d1d9e782ff8c86cba48e86ebe0d05e3c7f09f
Cristian Vacacela <cvacacelaprocel@cifpfbmoll.eu>
2025-05-11 05:20:53 +0200

    Added: declare environment variables

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+from pydantic_settings import BaseSettings
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
+
+class Settings(BaseSettings):
+    MONGODB_URL: str = os.getenv('MONGODB_URL')
+    MONGODB_NAME: str = os.getenv('MONGODB_NAME')
+    DEBUG_MODE: bool = os.getenv('DEBUG_MODE') == "True"
+    PORT: int = 8000
+    HOST: str = "0.0.0.0"
+
+settings = Settings()
