2aea175139987a3e364380e87ae830b4042f069e
Cristian Vacacela <cvacacelaprocel@cifpfbmoll.eu>
2025-05-11 05:13:03 +0200

    Added: build the app  and manage the connection

--- main.py
+++ main.py
@@ -1,10 +1,46 @@
-# all the imports
+import os
+import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from motor.motor_asyncio import AsyncIOMotorClient
+from starlette.responses import RedirectResponse
+from contextlib import asynccontextmanager
+from config import settings
+from routers import router
 
-# creating a server with python FastAPI
 app = FastAPI()
 
-# hello world endpoint
-@app.get("/")
-def read_root(): # function that is binded with the endpoint
-    return {"Hello": "World"}
+origins = ['http://localhost:3000']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.mongodb_client = AsyncIOMotorClient(settings.MONGODB_URL)
+    app.mongodb = app.mongodb_client[settings.MONGODB_NAME]
+    yield
+    app.mongodb_client.close()
+
+app = FastAPI(lifespan=lifespan)
+
+@app.get('/', include_in_schema=False)
+async def get_root():
+    root_url = os.environ.get('ROOT_URL')
+    return RedirectResponse(url=f"{root_url}/docs")
+
+app.include_router(router, tags=["Todo"], prefix="/api/v1/task")
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host=settings.HOST,
+        reload=settings.DEBUG_MODE,
+        port=settings.PORT,
+    )
